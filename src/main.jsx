d2794351bb92e606135ea8db7c78dc5109f9c437
alberto marcos <amarcosarranz@gmail.com>
2024-10-17 23:28:59 +0200

    estructura front

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AuthWrapper } from "./context/auth.context.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <AuthWrapper>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthWrapper>
+);
---
+++ src/pages/auth/Login.jsx
@@ -0,0 +1,79 @@
+import service from "../../services/config.js";
+
+import { AuthContext } from "../../context/auth.context.jsx";
+import { useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+
+export const Login = () => {
+  const navigate = useNavigate();
+  const { authenticateUser } = useContext(AuthContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    // ... contactar al backend para validar credenciales de usuario aqui
+    try {
+      const userCredentials = {
+        email,
+        password,
+      };
+
+      // const response = await axios.post("http://localhost:5005/api/auth/login", userCredentials)
+      const response = await service.post("/auth/login", userCredentials);
+
+      console.log(response);
+
+      localStorage.setItem("authToken", response.data.authToken);
+
+      await authenticateUser();
+
+      navigate("/private-page-example");
+    } catch (error) {
+      console.log(error);
+      if (error.response.status === 400) {
+        setErrorMessage(error.response.data.message);
+      } else {
+        //! aqui deberia haber redirección a /error
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h1>Formulario de Acceso</h1>
+
+      <form onSubmit={handleLogin}>
+        <label>Correo Electronico:</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+
+        <br />
+
+        <label>Contraseña:</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+
+        <br />
+
+        <button type="submit">Acceder</button>
+
+        {errorMessage && <p>{errorMessage}</p>}
+      </form>
+    </div>
+  );
+};
